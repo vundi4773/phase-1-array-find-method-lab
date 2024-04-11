9f6f064b7fb1ad44d902d745f86d8343c90bc505
vundi4773 <vundi4773@gmail.com>
2024-04-12 01:43:42 +0300

    complete lab

--- index.js
+++ index.js
@@ -4,16 +4,3 @@ function superbowlWin(record) {
   const winResult = record.find((record) => record.result === "W");
   return winResult ? winResult.year : undefined;
 }
-
-//     let result = record[result]
-//     let year = record[year]
-//   if ((result = "W"))
-//   return year;
-//   else (result = !"w")
-//   return "undefined";
-// }
-
-// function superbowlWin(record){
-//     const winResult = record.find(result = "W");
-//     return winResult ? winResult.year :undefined;
-// }
