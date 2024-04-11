bd5672c847f9f7d2751a2b5ec7932477227d80e5
vundi4773 <vundi4773@gmail.com>
2024-04-12 01:40:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function superbowlWin(record) {
+  const winResult = record.find((record) => record.result === "W");
+  return winResult ? winResult.year : undefined;
+}
+
+//     let result = record[result]
+//     let year = record[year]
+//   if ((result = "W"))
+//   return year;
+//   else (result = !"w")
+//   return "undefined";
+// }
+
+// function superbowlWin(record){
+//     const winResult = record.find(result = "W");
+//     return winResult ? winResult.year :undefined;
+// }
